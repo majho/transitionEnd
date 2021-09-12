b45fbde4c4ded13fe26e963c8986e71aa2632777
Martin Johansson <majho.butler@gmail.com>
2021-09-12 14:45:39 +0200

    removed old rollupjs config

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import babel from 'rollup-plugin-babel';
-
-export default {
-    entry: 'src/index.js',
-    moduleName: 'onTransitionEnd',
-    format: 'umd',
-    plugins: [babel()],
-    dest: 'dist/transition-end.js'
-};
